cd1224e6f824789aae6e19a68c76b30891335b92
Marko Đurđević <makimarkodjurdjevic@gmail.com>
2023-12-29 19:20:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ISIS_Project
+ Zadatak iz predmeta inteligentni softverski infrastrukturni sistemi.
---
+++ React_Frontend/client/src/App.tsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+
+function App()
+{
+    const [data, setData] = useState([{}]);
+
+    useEffect(() => {
+        fetch("/test").then(
+            res => res.json()
+        ).then(
+            data => {
+              setData(data)
+              console.log(data)
+            }
+        )
+    }, []);
+
+    return (
+            <div>
+              <p>{JSON.stringify(data)}</p>
+            </div>
+          );
+}
+
+export default App;
